7ec50429d080308f24b2553b2bbe2375304a7b06
Djenkaa <mihajlosmex@gmail.com>
2020-07-02 23:30:35 +0200

    google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,6 +50,15 @@
 
 @shared
 <script src="/js/all.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144192815-2"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-144192815-2');
+</script>
 
 
 @stack('js')
