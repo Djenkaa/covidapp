e93b539e0bd777dee90d96666860f9d68855e8f5
Djenkaa <mihajlosmex@gmail.com>
2020-07-11 10:40:37 +0200

    translate front

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -5,49 +5,35 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <!-- Brand -->
-        <a class="navbar-brand pt-0" href="{{ route('home') }}">
-{{--            <img src="{{ asset('argon') }}/img/brand/blue.png" class="navbar-brand-img" alt="...">--}}
-            <i class="fas fa-shield-virus fa-lg"></i> COVID-19
+        <a class="navbar-brand pt-0 text-uppercase" href="{{ route('home') }}">
+            <i class="fas fa-shield-virus fa-lg"></i> Covid-19
         </a>
         <!-- User -->
-{{--        <ul class="nav align-items-center d-md-none">--}}
-{{--            <li class="nav-item dropdown">--}}
-{{--                <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
-{{--                    <div class="media align-items-center">--}}
-{{--                        <span class="avatar avatar-sm rounded-circle">--}}
-{{--                        <img alt="Image placeholder" src="{{ asset('argon') }}/img/theme/team-1-800x800.jpg">--}}
-{{--                        </span>--}}
-{{--                    </div>--}}
-{{--                </a>--}}
-{{--                <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">--}}
-{{--                    <div class=" dropdown-header noti-title">--}}
-{{--                        <h6 class="text-overflow m-0">{{ __('Welcome!') }}</h6>--}}
-{{--                    </div>--}}
-{{--                    <a href="{{ route('profile.edit') }}" class="dropdown-item">--}}
-{{--                        <i class="ni ni-single-02"></i>--}}
-{{--                        <span>{{ __('My profile') }}</span>--}}
-{{--                    </a>--}}
-{{--                    <a href="#" class="dropdown-item">--}}
-{{--                        <i class="ni ni-settings-gear-65"></i>--}}
-{{--                        <span>{{ __('Settings') }}</span>--}}
-{{--                    </a>--}}
-{{--                    <a href="#" class="dropdown-item">--}}
-{{--                        <i class="ni ni-calendar-grid-58"></i>--}}
-{{--                        <span>{{ __('Activity') }}</span>--}}
-{{--                    </a>--}}
-{{--                    <a href="#" class="dropdown-item">--}}
-{{--                        <i class="ni ni-support-16"></i>--}}
-{{--                        <span>{{ __('Support') }}</span>--}}
-{{--                    </a>--}}
-{{--                    <div class="dropdown-divider"></div>--}}
-{{--                    <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();--}}
-{{--                    document.getElementById('logout-form').submit();">--}}
-{{--                        <i class="ni ni-user-run"></i>--}}
-{{--                        <span>{{ __('Logout') }}</span>--}}
-{{--                    </a>--}}
-{{--                </div>--}}
-{{--            </li>--}}
-{{--        </ul>--}}
+        <ul class="nav align-items-center d-md-none">
+            <li class="nav-item dropdown">
+                <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <div class="media align-items-center">
+                        <span class="badge badge-light">
+                            <img alt="Image placeholder" src="https://www.countryflags.io/gb/shiny/32.png"> <i class="fas fa-sort-down ml-1"></i>
+                            </span>
+                    </div>
+                </a>
+                <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
+                    <div class=" dropdown-header noti-title">
+                        <h6 class="text-overflow m-0">Languages</h6>
+                    </div>
+                    <a href="#" class="dropdown-item">
+                        <img src="https://www.countryflags.io/gb/shiny/24.png">
+                        <span> English</span>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <img src="https://www.countryflags.io/rs/shiny/24.png">
+                        <span> Serbian</span>
+                    </a>
+
+                </div>
+            </li>
+        </ul>
         <!-- Collapse -->
         <div class="collapse navbar-collapse" id="sidenav-collapse-main">
             <!-- Collapse header -->
@@ -55,8 +41,7 @@
                 <div class="row">
                     <div class="col-6 collapse-brand">
                         <a href="{{ route('home') }}">
-                            COVID-19
-{{--                            <img src="{{ asset('argon') }}/img/brand/blue.png">--}}
+                            <i class="fas fa-shield-virus"></i> Covid-19
                         </a>
                     </div>
                     <div class="col-6 collapse-close">
@@ -68,16 +53,16 @@
                 </div>
             </div>
             <!-- Form -->
-            <form class="mt-4 mb-3 d-md-none">
-                <div class="input-group input-group-rounded input-group-merge">
-                    <input type="search" class="form-control form-control-rounded form-control-prepended" placeholder="{{ __('Search') }}" aria-label="Search">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text">
-                            <span class="fa fa-search"></span>
-                        </div>
-                    </div>
-                </div>
-            </form>
+{{--            <form class="mt-4 mb-3 d-md-none">--}}
+{{--                <div class="input-group input-group-rounded input-group-merge">--}}
+{{--                    <input type="search" class="form-control form-control-rounded form-control-prepended" placeholder="{{ __('Search') }}" aria-label="Search">--}}
+{{--                    <div class="input-group-prepend">--}}
+{{--                        <div class="input-group-text">--}}
+{{--                            <span class="fa fa-search"></span>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </form>--}}
             <!-- Navigation -->
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -96,59 +81,11 @@
                         <i class="fas fa-plane-departure text-primary"></i> Travel Alert
                     </a>
                 </li>
-{{--                <li class="nav-item">--}}
-{{--                    <a class="nav-link active" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-examples">--}}
-{{--                        <i class="fab fa-laravel" style="color: #f4645f;"></i>--}}
-{{--                        <span class="nav-link-text" style="color: #f4645f;">{{ __('Laravel Examples') }}</span>--}}
-{{--                    </a>--}}
-
-{{--                    <div class="collapse show" id="navbar-examples">--}}
-{{--                        <ul class="nav nav-sm flex-column">--}}
-{{--                            <li class="nav-item">--}}
-{{--                                <a class="nav-link" href="{{ route('profile.edit') }}">--}}
-{{--                                    {{ __('User profile') }}--}}
-{{--                                </a>--}}
-{{--                            </li>--}}
-{{--                            <li class="nav-item">--}}
-{{--                                <a class="nav-link" href="{{ route('user.index') }}">--}}
-{{--                                    {{ __('User Management') }}--}}
-{{--                                </a>--}}
-{{--                            </li>--}}
-{{--                        </ul>--}}
-{{--                    </div>--}}
-{{--                </li>--}}
-
-{{--                <li class="nav-item">--}}
-{{--                    <a class="nav-link" href="#">--}}
-{{--                        <i class="ni ni-planet text-blue"></i> {{ __('Icons') }}--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--                <li class="nav-item">--}}
-{{--                    <a class="nav-link" href="#">--}}
-{{--                        <i class="ni ni-pin-3 text-orange"></i> {{ __('Maps') }}--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--                <li class="nav-item">--}}
-{{--                    <a class="nav-link" href="#">--}}
-{{--                        <i class="ni ni-key-25 text-info"></i> {{ __('Login') }}--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--                <li class="nav-item">--}}
-{{--                    <a class="nav-link" href="#">--}}
-{{--                        <i class="ni ni-circle-08 text-pink"></i> {{ __('Register') }}--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--                <li class="nav-item mb-5 bg-danger" style="position: absolute; bottom: 0;">--}}
-{{--                    <a class="nav-link text-white" href="https://www.creative-tim.com/product/argon-dashboard-pro-laravel" target="_blank">--}}
-{{--                        <i class="ni ni-cloud-download-95"></i> Upgrade to PRO--}}
-{{--                    </a>--}}
-{{--                </li>--}}
             </ul>
             <!-- Divider -->
             <hr class="my-3">
-            <!-- Heading -->
-{{--            <h6 class="navbar-heading text-muted">Documentation</h6>--}}
-{{--            <!-- Navigation -->--}}
+
+            <!-- Navigation -->
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('support')}}">
@@ -169,7 +106,6 @@
                 </li>
 
             </ul>
-
         </div>
     </div>
 </nav>
